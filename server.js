69065018891836c3ab22220f20972463a2a0b8f1
Indranil Chatterjee <indranil.chatterjee@pb.com>
2018-03-22 12:00:57 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const hbs = require('hbs');
+var fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, resp, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', err => {
+        if(err){
+            console.log(`Cannot write to logfile server.log.`)
+        }
+    });
+    next();
+});
+
+// app.use((req, resp, next) => {
+//     resp.render('maintenance.hbs');
+// });
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', text => text.toUpperCase());
+
+app.get('/', (req, resp) => {
+    resp.render('home.hbs', {
+        pageTitle: 'Home page',
+        welcomeMessage: 'Welcome to my website!'
+    });
+});
+
+app.get('/about', (req, resp) => {
+    resp.render('about.hbs', {
+        pageTitle: 'About page'
+    });
+});
+
+app.listen(3000, () => console.log('Server is up on port 3000!'));
